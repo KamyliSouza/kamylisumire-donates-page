d6b11d2023b150297faa935f3d96316bb84e08cd
Kamyli <92053318+KamyliSouza@users.noreply.github.com>
2026-09-08 13:56:17 -0300

    Atualizar o loader.js

--- js/core/loader.js
+++ js/core/loader.js
@@ -30,7 +30,7 @@
      * entrada antecipada da página antes do loader terminar.
      * Use este valor em vez de delay negativo.
      */
-    const PAGE_REVEAL_DELAY_MS = 150;
+    const PAGE_REVEAL_DELAY_MS = 0;
     const PAGE_REVEAL_OVERLAP_MS = 0;
 
     const REVEAL_STATE_MS = 420;
